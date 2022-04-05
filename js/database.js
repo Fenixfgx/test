a997a2cb9751c1e372f130affa147224af8f551f
Fenixfgx <72283013+Fenixfgx@users.noreply.github.com>
2022-04-05 18:35:40 -0500

    Add files via upload

---
+++ js/database.js
@@ -0,0 +1,7 @@
+[{"claves":"Buscador, Google, Asado","description":"Google","link":"http:\/\/www.google.com","title":"Google"}
+,{"claves":"Buscador, Bing, Pollo","description":"Bing","link":"http:\/\/www.bing.com","title":"Bing"}
+,{"claves":"Buscador, Segundo, Rata","description":"Test","link":"/Directorio/index.html","title":"DC"}
+
+
+
+]
