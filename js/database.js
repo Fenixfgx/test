3a97d30faf689cee10409308a854fde2153a6747
Fenixfgx <72283013+Fenixfgx@users.noreply.github.com>
2022-04-05 23:45:50 -0500

    Update database.js

--- js/database.js
+++ js/database.js
@@ -1,7 +1,12 @@
-[{"claves":"Buscador, Google, Asado","description":"Google","link":"http:\/\/www.google.com","title":"Google"}
-,{"claves":"Buscador, Bing, Pollo","description":"Bing","link":"http:\/\/www.bing.com","title":"Bing"}
-,{"claves":"Buscador, Segundo, Rata","description":"Test","link":"/Directorio/index.html","title":"DC"}
+[{"claves":"Break.net, Break, Angel Barragan, Breakstation, Internet, Provedor de Internet, Proveedor, Internet Rural, Internet para el campo, finca, campo, servicio de internet","description":"Proveedor de Servicio de Internet para el Hogar Urbano y Rural","link":"http:\/\/www.break.net","title":"Break.net"}
+,{"claves":"Telecomtrol, la mona, la monita, donde Diana, Papeleria, internet, fotocopias, escaner, recargas, tareas, trabajos, laminación, laminar, revistas, , ","description":"Papeleria, Internet, Recargas, Fotocopias y mucho mas...","link":"/telecomtrol.html","title":"Telecomtrol"}
+,{"claves":"Rapizzpollo, Rapispollo, almuerzo, alimentos, almuerzos, pollo asado, comida, alitas de pollo, pollo broster, pollo broaster, Pollo","description":"Asadero de pollo","link":"/Rapizzpollo.html","title":"Rapizzpollo"}
+,{"claves":"Las Brasas de Cachipay, Brasas, almuerzo, alimentos, almuerzos, comida, pollo asado, alitas de pollo, pollo broster, pollo broaster, Pollo","description":"Asadero de pollo","link":"/brasasdecachipay.html","title":"Las Brasas de Cachipay"}
+,{"claves":"Restaurante San Angelo, San Angelo, almuerzo, alimentos, Restaurante, restaurante italiano, restaurante familiar, pasta, espagueti, espaghetti, lasagna, lasaña, comida, pollo, alitas de pollo, pollo broster, Pollo","description":"Restaurante Italiano","link":"/sanangelo.html","title":"San Angelo"}
+,{"claves":"Restaurante Los Balcones, Los balcones, almuerzo, alimentos, Restaurante, restaurante italiano, restaurante familiar, comida, pollo, alitas de pollo, pollo broaster, Sopa, Pollo","description":"Restaurante Familiar","link":"/losbalcones.html","title":"Los Balcones"}
+,{"claves":"Cootrasan, Camionetas, taxis, travel, viajar, transporte, paqueteria, mensajeria, viajes, bogota, cartagenita, las camionetas","description":"Viaje Seguro","link":"/Cootrasan.html","title":"Cootrasan"}
+,{"claves":"Expreso el Sol, Buses, transporte, viajar, tiquetes, viajes, paqueteria, travel","description":"Linea de autobuses","link":"/expresoelsol.html","title":"Expreso el Sol"}
+,{"claves":"Ferreteria Don Oscar, Oscar, Ferretería, ladrillos, perfiles, tornillos, varillas, cemento,","description":"Ferreteria y Construccion","link":"/ferreteriadonoscar.html","title":"Ferreteria Don Oscar"}
+,{"claves":"Veterinaria NN, mascotas, animales, concentrados, peces, perros, gatos, caballos medicina, ","description":"Al cuidado de las Mascotas","link":"/veterinaria.html","title":"Veterinaria NN"}
+,{"claves":"Contegral, Cointegral, mascotas, animales, concentrados, peces, medicina, ","description":"Concentrado para Animales","link":"/contegral.html","title":"Contegral"}
 
-
-
-]
